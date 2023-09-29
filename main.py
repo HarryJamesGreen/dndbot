3c4e3d04d209b7ecf3320e785520333f747bc2c6
HarryJamesGreen <hh345@canterbury.ac.uk>
2023-09-29 05:11:03 +0100

    Big update

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
+
+from src.csv_exporter import export_to_csv
 from src.screen_capture import capture_screen
 from src.text_extraction import extract_text_from_image
-from src.csv_exporter import export_to_csv
-import pytesseract
 import datetime
 import pygetwindow as gw
-import time
-import pyautogui
-import random
-from src.mouse_interaction import jiggle_mouse
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  # Adjust the path accordingly
 
 def main():
     # Define the headers for the CSV
-    headers = ['Timestamp', 'Message']
 
     # Get the window by its title
     window_title = 'Dark and Darker'
-    window = gw.getWindowsWithTitle(window_title)[0]
 
-    # Define the region to capture the left half of the window
-    region = (window.left, window.top, 640, 720)
+    # Use getWindowsWithTitles to find the window
+    windows = gw.getWindowsWithTitle(window_title)
 
-    while True:
-        # Capture the specified region
-        screenshot = capture_screen(region)
+    if windows:
+        # If the window is found, use the first window in the list
+        window = windows[0]
 
-        # Extract text from the screenshot
-        text = extract_text_from_image(screenshot)
+        # Define the region to capture the left half of the window
+        region = (window.left, window.top, window.width // 2, window.height)
 
-        # Process the extracted text
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        messages = text.splitlines()
-        data = [[timestamp, message] for message in messages]
+        while True:
+            print(window, region)
+            # Capture the specified region
+            screenshot = capture_screen(region)
 
-        # Export the data to a CSV file
-        export_to_csv(data, headers)
+            # Extract text from the screenshot
+            text = extract_text_from_image(screenshot)
+            print(text)
+            # Process the extracted text
+            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            messages = text.splitlines()
+            data = [[timestamp, message] for message in messages]
+
+            # Export the data to a CSV file
+            export_to_csv(data, "output.csv")
+            print(text)
 
 
 if __name__ == "__main__":
--- src/text_extraction.py
+++ src/text_extraction.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import pytesseract
 
+
 def extract_text_from_image(image):
     """
     Extracts text from a given image.
